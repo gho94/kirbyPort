1dcd4cdc5115572ebfa18a1140eb69db71169460
이예지 <yeji0718@naver.com>
2024-11-19 15:10:21 +0900

    [Design] ui 구현
    1. 채팅방 목록 ui 구현
    2. 채팅방 추가 스크린 ui 구현

--- kirby_port_app/lib/screen/home_screen.dart
+++ kirby_port_app/lib/screen/home_screen.dart
@@ -31,9 +31,29 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("방 목록"),
+        title: const Text(
+          "KriBy - Port",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
         centerTitle: true,
-        actions: [IconButton(icon: const Icon(Icons.filter_alt), onPressed: () => context.push("/topic-filter"))],
+        actions: [
+          IconButton(
+              icon: Icon(
+                Icons.filter_alt_outlined,
+                color: Colors.red[900],
+                size: 30,
+              ),
+              onPressed: () => context.push("/topic-filter")),
+          IconButton(
+              icon: Icon(
+                Icons.add,
+                color: Colors.red[900],
+                size: 30,
+              ),
+              onPressed: () => context.push("/create-room")),
+        ],
       ),
       body: PageView(
         controller: _pageController,
